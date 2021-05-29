ce6b762ac99a6a71dbf6355b0e2d6422bd7840d4
shpagZZZa <56957632+shpagZZZa@users.noreply.github.com>
2021-05-29 19:38:27 +0500

    lab5

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace Lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(64, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(611, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(64, 539);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1047, 289);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1204, 1055);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5
+{
+    public partial class Form1 : Form
+    {
+        private Image image = Image.FromFile("../../rpuxa.jpg");
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            pictureBox1.Image = image;
+            pictureBox2.Image = ConvertImage(image);
+            FillDataGrid(GetBitmapFromImage(image));
+        }
+
+        private Image ConvertImage(Image image)
+        {
+            Bitmap bitmap = new Bitmap(GetBitmapFromImage(image));
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    Color convertedPixel = Negative(bitmap.GetPixel(x, y));
+                    bitmap.SetPixel(x, y, convertedPixel);
+                }
+            }
+            return bitmap;
+        }
+
+        private void FillDataGrid(Bitmap bitmap)
+        {
+            int width = bitmap.Width;
+            int height = bitmap.Height;
+            string[][] rows = new string[height][];
+
+            dataGridView1.ColumnCount = width;
+
+            
+            for (int y = 0; y < height; y++)
+            {
+                string[] row = new string[width];
+                for (int x = 0; x < width; x++)
+                {
+                    row[x] = ColorToString(bitmap.GetPixel(x, y));
+                }
+                rows[y] = row;
+            }
+
+            Console.WriteLine(rows);
+            foreach (string[] row in rows)
+            {
+                dataGridView1.Rows.Add(row);
+            }
+        }
+     
+        private string ColorToString(Color color)
+        {
+            return String.Format("{0} - {1} - {2}", color.R, color.G, color.B);
+        }
+
+        // занято
+        private Color Negative(Color color)
+        {
+            return Color.FromArgb(255 - color.R, 255 - color.G, 255 - color.B);
+        }
+
+        private Color Absolutize(Color color)
+        {
+            int red = color.R;
+            int green = color.G;
+            int blue = color.B;
+            if (blue > red && blue > green)
+            {
+                return Color.FromArgb(0, 0, 255);
+            }
+            if (red > blue && red > green)
+            {
+                return Color.FromArgb(255, 0, 0);
+            }
+            if (green > red && green > red)
+            {
+                return Color.FromArgb(0, 255, 0);
+            }
+
+            return color;
+        }
+
+        private Color Dark(Color color)
+        {
+            int value = (color.R + color.G + color.B) / 3;
+
+            return Color.FromArgb(value, value, value);
+        }
+
+
+
+        private Bitmap GetBitmapFromImage(Image image)
+        {
+            return (Bitmap)image;
+        }
+    }
+}
